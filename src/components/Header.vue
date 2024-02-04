b9e44f741701b6d65ec2ae86b6eeb401fd9f2012
Fabricio-Guima <fsgkof@gmail.com>
2024-02-04 20:00:40 -0300

    Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/" class="logo">
+        <img src="@/assets/svg/ranek.svg" alt="Ranek ecommerce" />
+      </RouterLink>
+      <RouterLink class="btn" to="/login">Vender / Login</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 20px;
+  box-shadow: 0 2px 4px rgba(30, 60, 90, 0.1);
+}
+
+.logo {
+  padding: 10px 0;
+}
+
+.logo img {
+  width: 90px;
+}
+</style>
