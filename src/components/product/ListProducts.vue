74a1b84a9c00274bebfce182c7f1fc5ce97379c7
Fabricio-Guima <fsgkof@gmail.com>
2024-02-05 09:05:04 -0300

    get products using axios

--- src/components/product/ListProducts.vue
+++ src/components/product/ListProducts.vue
@@ -1,9 +1,33 @@
 <template>
-  <div>
-    <h1>Lista de produtos</h1>
-  </div>
+  <section class="produtos-container">
+    <div v-for="product in products" :key="product.id">
+      <img
+        v-if="product.fotos && product.fotos.length > 0"
+        :src="product.fotos[0]"
+        :alt="product.fotos[0].titulo"
+      />
+      <p class="preco">{{ product.preco }}</p>
+      <h2 class="titulo">{{ product.nome }}</h2>
+      <p>{{ product.description }}</p>
+    </div>
+  </section>
 </template>
 
-<script setup></script>
+<script setup>
+import { onBeforeMount, ref } from "vue";
+import { api } from "@/services/api.js";
+
+const products = ref([]);
+
+const getProducts = () => {
+  api.get("/produtos").then((response) => {
+    products.value = response.data;
+  });
+};
+
+onBeforeMount(() => {
+  getProducts();
+});
+</script>
 
 <style lang="scss" scoped></style>
