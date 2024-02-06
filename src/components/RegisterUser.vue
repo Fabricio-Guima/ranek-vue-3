0f853da9b25fc8042ac94a6d7aece8456c3b7edd
Fabricio-Guima <fsgkof@gmail.com>
2024-02-06 15:10:37 -0300

    register user component

---
+++ src/components/RegisterUser.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Register user</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.login {
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+</style>
