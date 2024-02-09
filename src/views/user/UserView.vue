3db19d376e58ba1bb0ae8ca5452ddd53709263ba
Fabricio-Guima <fsgkof@gmail.com>
2024-02-09 20:37:12 -0300

    user page

---
+++ src/views/user/UserView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>View de User</h1>
+    <p>{{ userStore.user }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from "@/stores/user";
+
+const userStore = useUserStore();
+</script>
+
+<style scoped></style>
