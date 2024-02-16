f01c6126c8cf3bf7c684b36bc97e83c3e111177f
Fabricio-Guima <fsgkof@gmail.com>
2024-02-15 21:40:45 -0300

    children route user

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ProductsUserView from "../views/user/ProductsUserView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,8 +25,30 @@ const router = createRouter({
     {
       path: "/users",
       name: "user",
-
       component: () => import("../views/user/UserView.vue"),
+
+      children: [
+        {
+          path: "",
+          name: "user.products",
+          component: () => import("../views/user/ProductsUserView.vue"),
+        },
+        {
+          path: "edit",
+          name: "user.edit",
+          component: () => import("../views/user/EditUserView.vue"),
+        },
+        {
+          path: "purchases",
+          name: "user.purchases",
+          component: () => import("../views/user/PurchasesUserView.vue"),
+        },
+        {
+          path: "sales",
+          name: "user.sales",
+          component: () => import("../views/user/SalesUserView.vue"),
+        },
+      ],
     },
   ],
 });
