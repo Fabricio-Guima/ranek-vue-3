9e05bb6a0d080fc4f7893a918aa1ea2b05917060
Fabricio-Guima <fsgkof@gmail.com>
2024-02-15 21:42:02 -0300

    ProductUserView

---
+++ src/views/user/ProductsUserView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Lista de Produtos</p>
+    <p v-if="userStore.user">
+      {{ userStore.user }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from "@/stores/user";
+const userStore = useUserStore();
+</script>
+
+<style lang="scss" scoped></style>
