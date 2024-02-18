159cb6c7789e5e066b80fc8cd559b5d622ab1529
Fabricio-Guima <fsgkof@gmail.com>
2024-02-18 15:04:04 -0300

    get products from user and show them

--- src/views/user/ProductsUserView.vue
+++ src/views/user/ProductsUserView.vue
@@ -1,15 +1,36 @@
 <template>
-  <div>
-    <p>Lista de Produtos</p>
-    <p v-if="userStore.user">
-      {{ userStore.user }}
-    </p>
-  </div>
+  <section>
+    <h2>Adicionar Produtos</h2>
+    <AddProductVue />
+    <h2>Seus Produtos</h2>
+    <transition-group v-if="userStore.userProducts" name="list" tag="ul">
+      <li v-for="(product, index) in userStore.userProducts" :key="index">
+        <ItemProduct :product="product">
+          <p>{{ product.descricao }}</p>
+        </ItemProduct>
+      </li>
+    </transition-group>
+  </section>
 </template>
 
 <script setup>
+import { onBeforeMount, watch } from "vue";
 import { useUserStore } from "@/stores/user";
+import AddProductVue from "@/components/product/AddProduct.vue";
+import ItemProduct from "@/components/product/ItemProduct.vue";
 const userStore = useUserStore();
+
+// watch(userStore.isLogged, () => {
+//   userStore.getUserProducts();
+// });
+
+onBeforeMount(() => {
+  if (userStore.isLogged) userStore.getUserProducts();
+});
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+}
+</style>
