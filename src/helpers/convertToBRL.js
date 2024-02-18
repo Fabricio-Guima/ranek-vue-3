972c5ef6ce3f3c3fb2612b5e0510907e430449db
Fabricio-Guima <fsgkof@gmail.com>
2024-02-18 15:03:01 -0300

    formatted price in brl

--- src/helpers/convertToBRL.js
+++ src/helpers/convertToBRL.js
@@ -1,6 +1,9 @@
-export function formatNumberToPtBr(price) {
-  return `R$ ${price.toLocaleString("pt-BR", {
+export function formattedPrice(price) {
+  const numberFormat = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
-  })}`;
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+  return numberFormat.format(price);
 }
