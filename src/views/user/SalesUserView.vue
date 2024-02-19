a605b0a9c8c2ca6cf020eb0cfd84c67ded45c9b4
Fabricio-Guima <fsgkof@gmail.com>
2024-02-19 17:03:30 -0300

    SalesUserView

--- src/views/user/SalesUserView.vue
+++ src/views/user/SalesUserView.vue
@@ -1,7 +1,68 @@
 <template>
-  <p>Vendas do Usuário</p>
+  <section>
+    <div v-if="sales">
+      <h2>Vendas</h2>
+      <div class="produtos-wrapper" v-for="(sale, index) in sales" :key="index">
+        <ItemProduct v-if="sale.product" :product="sale.product">
+          <p class="vendedor">
+            <span>Compradores </span> {{ sale.purchaser_id }}
+          </p>
+        </ItemProduct>
+        <div class="entrega">
+          <h3>Entrega:</h3>
+          <ul v-if="sale.endereco">
+            <li v-for="(value, index) in sale.endereco" :key="index">
+              {{ index }}: {{ value }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup></script>
+<script setup>
+import { onBeforeMount, ref } from "vue";
+import { useUserStore } from "@/stores/user";
+import ItemProduct from "@/components/product/ItemProduct.vue";
+import { api } from "@/services/api";
 
-<style lang="scss" scoped></style>
+const userStore = useUserStore();
+const sales = ref(null);
+
+const getSales = () => {
+  api.get(`/transacao?saller_id=${userStore.user.id}`).then((response) => {
+    sales.value = response.data;
+  });
+};
+
+onBeforeMount(() => {
+  if (userStore.isLogged) {
+    getSales();
+  }
+});
+</script>
+
+<style scoped>
+.produto-wrapper {
+  margin-bottom: 40px;
+}
+.vendedor span {
+  color: #e80;
+}
+.entrega {
+  display: grid;
+  grid-template-columns: minmax(100px, 200px) 1fr;
+  gap: 20px;
+  margin-bottom: 60px;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+
+h3 {
+  margin: 0;
+  justify-self: end;
+}
+</style>
