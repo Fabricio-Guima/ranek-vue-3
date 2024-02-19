a9d9778b4c5372b9c72b8ca5211174351bbf8789
Fabricio-Guima <fsgkof@gmail.com>
2024-02-19 17:03:20 -0300

    PurschaseUserView

--- src/views/user/PurchasesUserView.vue
+++ src/views/user/PurchasesUserView.vue
@@ -1,7 +1,49 @@
 <template>
-  <p>Compras do Usuário</p>
+  <section>
+    <div v-if="purchases">
+      <h2>Compras</h2>
+      <div
+        class="produtos-wrapper"
+        v-for="(purchase, index) in purchases"
+        :key="index"
+      >
+        <ItemProduct v-if="purchase.product" :product="purchase.product">
+          <p class="vendedor">
+            <span>Vendedor </span> {{ purchase.saller_id }}
+          </p>
+        </ItemProduct>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup></script>
+<script setup>
+import { onBeforeMount, ref } from "vue";
+import { useUserStore } from "@/stores/user";
+import ItemProduct from "@/components/product/ItemProduct.vue";
+import { api } from "@/services/api";
 
-<style lang="scss" scoped></style>
+const userStore = useUserStore();
+const purchases = ref(null);
+
+const getPurchases = () => {
+  api.get(`/transacao?purchaser_id=${userStore.user.id}`).then((response) => {
+    purchases.value = response.data;
+  });
+};
+
+onBeforeMount(() => {
+  if (userStore.isLogged) {
+    getPurchases();
+  }
+});
+</script>
+
+<style scoped>
+.produto-wrapper {
+  margin-bottom: 40px;
+}
+.vendedor span {
+  color: #e80;
+}
+</style>
